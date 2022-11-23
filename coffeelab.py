24823026f730a404b91ed2f6c9d916cc0dd48847
travm6060 <travm6060@gmail.com>
2022-11-22 21:01:46 -0700

    testing my first commit with homework

---
+++ coffeelab.py
@@ -0,0 +1,33 @@
+import sys
+customer = 'blank'
+agentJpref = 'coffee'
+agentQpref = 'decaf coffee'
+agentSpref = 'coffee'
+agentMpref = 'tea'
+
+coffee_available = False
+tea_available = True
+decaf_available = True
+argv = customer
+
+if sys.argv[2] == 'J':
+    if coffee_available == True:
+        print("Agent J prefers coffee and we have plenty")
+    else:
+        print("Agent J prefers coffee, but we are out, Agent J will have tea instead")
+if sys.argv[2] == 'Q':
+    if decaf_available == True:
+        print("Agent Q prefers decaf coffee and we have plenty")
+    else:
+        print("Agent Q prefers decaf coffee but we are out of decaf")
+if sys.argv[2] == 'S':
+    if coffee_available == True:
+        print("Agent S prefers coffee and we have plenty")
+    else:
+        print("Agent S prefers coffee but we are out, so Agent S will have decaf instead")
+if sys.argv[2] == 'M':
+    if tea_available == True:
+        print("Agent M prefers tea and we have plenty")
+    else:
+        print("Agent M prefers tea but we are out")
+
