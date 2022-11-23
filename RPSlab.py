61babd733972d3869c7e58acb13570dbed69be25
travm6060 <44046458+travm6060@users.noreply.github.com>
2022-11-23 11:39:52 -0700

    Create RPSlab.py

---
+++ RPSlab.py
@@ -0,0 +1,34 @@
+import sys
+import random
+
+arguments = sys.argv[1]
+
+def pick_rps(list):
+    return random.choice(list)
+
+word_list = ['rock', 'paper', 'scissors']
+
+print("you pick " + arguments)
+comp_rps = pick_rps(word_list)
+print("computer picks " + comp_rps)
+
+if arguments  == comp_rps:
+    print("you tied")
+
+if arguments == 'scissors':
+    if comp_rps == 'rock':
+        print("Computer Wins")
+    elif comp_rps == 'paper':
+        print("You Win!")
+        
+if arguments == 'rock':
+    if comp_rps == 'paper':
+        print("Computer Wins")
+    elif comp_rps == 'scissors':
+        print("You Win!")
+
+if arguments == 'paper':
+    if comp_rps == 'scissors':
+        print("Computer Wins")
+    elif comp_rps == 'rock':
+        print("You Win!")
